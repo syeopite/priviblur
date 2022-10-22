0efc2b6c1dc8475a143c724fdffac71a7a3693c9
syeopite <syeopite@syeopite.dev>
2022-10-22 02:20:07 -0700

    Change calls of privblur-api to privblur-extractor
    Library has been renamed

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from sanic import Sanic
 import sanic.response
 
-import privblur_api
+import privblur_extractor
 import orjson
 import logging
 
@@ -15,7 +15,7 @@ app = Sanic("Privblur", loads=orjson.loads, dumps=orjson.dumps)
 async def initialize(app):
     # We use the default client for now. But in the future, we'll pass in our own custom
     # aiohttp client when the need arises for it.
-    app.ctx.TumblrAPI = await privblur_api.TumblrAPI.create(json_loads=orjson.loads)
+    app.ctx.TumblrAPI = await privblur_extractor.TumblrAPI.create(json_loads=orjson.loads)
 
 
 @app.get("/")
