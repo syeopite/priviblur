8439549505ef7dcf377d16a571aedc340fc982de
syeopite <syeopite@syeopite.dev>
2022-10-21 21:05:21 -0700

    Initial commit

---
+++ src/version.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+# Taken from the Invidious Project
+CURRENT_BRANCH = subprocess.run(
+    "git branch --show-current".split(),
+    stdout=subprocess.PIPE
+).stdout.decode("utf-8").strip()
+
+CURRENT_COMMIT = subprocess.run(
+    "git rev-list HEAD --max-count=1 --abbrev-commit".split(),
+    stdout=subprocess.PIPE
+).stdout.decode("utf-8").strip()
+
+CURRENT_VERSION = subprocess.run(
+    "git log -1 --format=%ci".split(),
+    stdout=subprocess.PIPE
+).stdout.decode("utf-8").split()[0].replace("-", ".")
+
+
+VERSION = f"{CURRENT_VERSION}-{CURRENT_COMMIT} @ {CURRENT_BRANCH}"
