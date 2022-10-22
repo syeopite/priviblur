d1a884a294ac173fb8bde9a6488d2f85e10342bc
syeopite <syeopite@syeopite.dev>
2022-10-22 00:12:50 -0700

    Setup basic sanic server

---
+++ src/server.py
@@ -0,0 +1,27 @@
+from sanic import Sanic
+import sanic.response
+
+import privblur_api
+import orjson
+import logging
+
+from src.version import VERSION
+
+
+app = Sanic("Privblur", loads=orjson.loads, dumps=orjson.dumps)
+
+
+@app.listener("before_server_start")
+async def initialize(app):
+    # We use the default client for now. But in the future, we'll pass in our own custom
+    # aiohttp client when the need arises for it.
+    app.ctx.TumblrAPI = await privblur_api.TumblrAPI.create(json_loads=orjson.loads)
+
+
+@app.get("/")
+async def root(request):
+    return sanic.response.text(VERSION)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
