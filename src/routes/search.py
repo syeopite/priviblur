b4052bbd8a4a3d1aa963eb88a5fba7f9ae9c6af9
syeopite <syeopite@syeopite.dev>
2023-01-04 23:23:26 -0800

    Allow / in search queries

--- src/routes/search.py
+++ src/routes/search.py
@@ -30,7 +30,7 @@ async def render_results(app, initial_results, query, url_handler):
 async def query_param_redirect(request: sanic.Request): 
     """Endpoint for /search to redirect q= queries to /search/<query>"""
     if query := request.args.get("q"):
-        return sanic.redirect(request.app.url_for("search._main", query=urllib.parse.quote(query)))
+        return sanic.redirect(request.app.url_for("search._main", query=urllib.parse.quote(query, safe="~")))
     else:
         return sanic.redirect(request.app.url_for("explore._trending"))
 
