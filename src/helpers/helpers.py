05950af6b521f5eacfbee2f30da1d600f197911a
syeopite <syeopite@syeopite.dev>
2023-11-13 15:13:20 -0800

    Replace href.li links with their destination

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -7,29 +7,32 @@ def url_handler(raw_url):
 
     hostname = url.hostname
 
-    if hostname.endswith("64.media.tumblr.com"):
-        return f"/tblr/media/64{url.path}"
-    elif hostname.endswith("assets.tumblr.com"):
-        return f"/tblr/assets{url.path}"
-    elif hostname.endswith("49.media.tumblr.com"):
-        return f"/tblr/media/49{url.path}"
-    elif hostname.endswith("44.media.tumblr.com"):
-        return f"/tblr/media/44{url.path}"
+    if hostname.endswith("href.li"):
+        return url_handler(url.query)
     elif hostname.endswith("tumblr.com"):
-        # Check for subdomain blog
-        sub_domains = hostname.split(".")
-
-        if sub_domains[0] == "www":
-            potential_blog_name = sub_domains[1]
+        if hostname.endswith("64.media.tumblr.com"):
+            return f"/tblr/media/64{url.path}"
+        elif hostname.endswith("assets.tumblr.com"):
+            return f"/tblr/assets{url.path}"
+        elif hostname.endswith("49.media.tumblr.com"):
+            return f"/tblr/media/49{url.path}"
+        elif hostname.endswith("44.media.tumblr.com"):
+            return f"/tblr/media/44{url.path}"
         else:
-            potential_blog_name = sub_domains[0]
+            # Check for subdomain blog
+            sub_domains = hostname.split(".")
 
-        # Check if blog
-        if potential_blog_name != "tumblr":
-            if url.path.startswith("/post"):
-                return f"/{potential_blog_name}{url.path[5:]}"
-        else:
-            return f"{url.path}"
+            if sub_domains[0] == "www":
+                potential_blog_name = sub_domains[1]
+            else:
+                potential_blog_name = sub_domains[0]
+
+            # Check if blog
+            if potential_blog_name != "tumblr":
+                if url.path.startswith("/post"):
+                    return f"/{potential_blog_name}{url.path[5:]}"
+            else:
+                return f"{url.path}"
 
     return raw_url
 
