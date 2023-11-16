d48dae9cc8bfb5156612777eea3a8eba6c397b0f
syeopite <syeopite@syeopite.dev>
2023-11-16 14:52:45 -0800

    Add sorting options to the /tagged/<tag> route

--- src/routes/tagged.py
+++ src/routes/tagged.py
@@ -11,32 +11,34 @@ from .. import privblur_extractor
 tagged = sanic.Blueprint("tagged", url_prefix="/tagged")
 
 
-async def render_results(app, initial_results, tag, url_handler):
-        timeline = privblur_extractor.parse_container(initial_results)
-
-        return await sanic_ext.render(
-            "tagged.jinja",
-            context={
-                "app": app,
-                "html_escape": html.escape,
-                "url_escape": urllib.parse.quote,
-                "timeline": timeline,
-                "tag": tag,
-                "url_handler": url_handler,
-                "format_npf": npf_renderer.format_npf
-            }
-        )
-
 @tagged.get("/<tag:str>")
 async def _main(request: sanic.Request, tag: str):
     tag = urllib.parse.unquote(tag)
+    sort_by = time_filter = request.args.get("sort") or "top"
 
     if continuation := request.args.get("continuation"):
         continuation = urllib.parse.unquote(continuation)
 
-    initial_results = await request.app.ctx.TumblrAPI.hubs_timeline(
-        tag,
-        continuation=continuation
-    )
+    if sort_by == "top":
+        latest = False
+    else:
+        latest = True
 
-    return await render_results(request.app, initial_results, tag, request.app.ctx.URL_HANDLER)
+    initial_results = await request.app.ctx.TumblrAPI.hubs_timeline(tag, continuation=continuation, latest=latest)
+
+    timeline = privblur_extractor.parse_container(initial_results)
+
+    return await sanic_ext.render(
+        "tagged.jinja",
+        context={
+            "app": request.app,
+            "query_args": request.args,
+            "html_escape": html.escape,
+            "url_escape": urllib.parse.quote,
+            "timeline": timeline,
+            "tag": tag,
+            "url_handler": request.app.ctx.URL_HANDLER,
+            "format_npf": npf_renderer.format_npf,
+            "sort_by": sort_by
+        }
+    )
