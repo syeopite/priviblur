8431dbcc328792c1ddd9dd74f0853df7ba2f62e2
syeopite <syeopite@syeopite.dev>
2023-12-12 19:06:36 -0800

    Return error status codes on error handlers

--- src/helpers/error_handlers.py
+++ src/helpers/error_handlers.py
@@ -9,7 +9,8 @@ async def tumblr_error_login_walled(request, exception):
             "exception": exception,
             "error_heading": request.app.ctx.translate("en", "tumblr_error_blog_login_required_error_heading"),
             "error_description": request.app.ctx.translate("en", "tumblr_error_blog_login_required_error_description"),
-        }
+        },
+        status=403
     )
 
 
@@ -21,7 +22,8 @@ async def tumblr_error_restricted_tag(request, exception):
             "exception": exception,
             "error_heading": request.app.ctx.translate("en", "tumblr_error_restricted_tag_error_heading"),
             "error_description": request.app.ctx.translate("en", "tumblr_error_restricted_tag_description"),
-        }
+        },
+        status=403
     )
 
 async def tumblr_error_unknown_blog(request, exception):
@@ -32,7 +34,8 @@ async def tumblr_error_unknown_blog(request, exception):
             "exception": exception,
             "error_heading": request.app.ctx.translate("en", "tumblr_error_blog_not_found_error_heading"),
             "error_description": request.app.ctx.translate("en", "tumblr_error_blog_not_found_error_description"),
-        }
+        },
+        status=404
     )
 
 
@@ -70,5 +73,6 @@ async def error_404(request, exception):
             "exception": exception,
             "error_heading": "404: Not Found",
             "error_description": f"The requested URL \"{request.path}\" was not found",
-        }
+        },
+    status=404
     )
