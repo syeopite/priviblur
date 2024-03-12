0b163b36af608ca36e07f1e5206763d51df79754
syeopite <syeopite@syeopite.dev>
2024-03-11 23:15:46 -0700

    Add polish to dropdown behavior
     - Reduce transition time
     - Make <a> affect entire width of option instead of just text
     - Toggle dropdown with the dropdown symbol as well

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -5,13 +5,25 @@ let current_dropdown = null;
 const control_bar_actions = document.getElementsByClassName("control-bar-action");
 for (let action of control_bar_actions) {
     action.classList.remove("no-js");
+    let dropdown = action.getElementsByClassName("control-bar-dropdown-menu")[0]
 
     action.addEventListener("click", function(e) {
-        if (current_dropdown && current_dropdown != e.target) {
+
+        // Get the root element of the currently clicked dropdown
+        // This is used to check if the element we've clicked on is apart of the currently active dropdown
+        // and if so disable it (unless its part of the options)
+        let target;
+        if (e.target != action) {
+            target = e.target.closest(".control-bar-action")
+        } else {
+            target = action
+        }
+
+        if (current_dropdown && current_dropdown != target) {
             current_dropdown.classList.remove("active-dropdown-menu")
         };
 
-        if (action.classList.contains("active-dropdown-menu")) {
+        if (action.classList.contains("active-dropdown-menu") && !dropdown.contains(e.target)) {
             current_dropdown = null;
             action.classList.remove("active-dropdown-menu")
         } else {
