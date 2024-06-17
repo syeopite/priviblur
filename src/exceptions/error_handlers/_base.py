0231fa869edacb05f84b1d914ad981b1e70cbd55
syeopite <70992037+syeopite@users.noreply.github.com>
2024-06-17 18:44:25 +0000

    Add basic fallback error handler (#46)
    This commit adds a proper basic fallback error handler that shows debeug information and the context of when the error has occured.
    
    This replaces the default Sanic error handler which does not show any debug information when development mode is off.
    
    The styling for the error handler is very basic and serves as a placeholder until a proper design can be done for it.
    
    This will likely be a v0.3.0 feature rather than v0.1.0 one.

--- src/exceptions/error_handlers/_base.py
+++ src/exceptions/error_handlers/_base.py
@@ -1,3 +1,5 @@
+import inspect
+
 class ErrorHandlerGroup:
     """Manages and registers error handler functions for specific exceptions
 
@@ -24,3 +26,35 @@ class ErrorHandlerGroup:
         for handler, exceptions in self.registered_handlers.items():
             for exception in exceptions:
                 app.error_handler.add(exception, handler)
+
+
+def create_user_friendly_error_message(request, exception):
+    # Taken from https://github.com/searxng/searxng/blob/f5eb56b63f250c7804e5e1cf4426e550bc933906/searx/metrics/error_recorder.py
+    exception_class = exception.__class__
+    exception_name = exception_class.__qualname__
+    exception_module = exception_class.__module__
+
+    if exception_module is None or exception_module == str.__class__.__module__:
+        processed_exception_name = exception_name
+    else:
+        processed_exception_name = f"{exception_module}.{exception_name}"
+
+    exception_message = str(exception) or None
+
+    frame = inspect.trace()
+    context = []
+
+    for trace in reversed(frame):
+        if trace.filename.startswith(request.app.ctx.PRIVIBLUR_PARENT_DIR_PATH):
+            local_path = trace.filename[len(request.app.ctx.PRIVIBLUR_PARENT_DIR_PATH) + 1:]
+        else:
+            local_path = trace.filename
+        occurrence = f"File \"{local_path}\" line {trace.lineno}: in {trace.function}"
+
+        # Add code context is applicable
+        if trace.code_context:
+            occurrence = f"{occurrence}\n  {trace.code_context[0].strip()}"
+
+        context.append(occurrence)
+
+    return processed_exception_name, exception_message, '\n'.join(context)
