3ff74bd1d1edb5d5ef07e3d5b8b70da824928a44
syeopite <syeopite@syeopite.dev>
2024-09-24 16:22:42 -0700

    Simplify parse_* functions

--- src/priviblur_extractor/parse/base.py
+++ src/priviblur_extractor/parse/base.py
@@ -1,16 +1,13 @@
 from . import collection_parsers
 
 def parse_timeline(initial_data):
-    initial_data = initial_data["response"]
-    return collection_parsers.TimelineParser.process(initial_data)
+    return collection_parsers.TimelineParser.process(initial_data["response"])
 
 
 def parse_blog_timeline(initial_data):
-    initial_data = initial_data["response"]
-    return collection_parsers.BlogParser.process(initial_data)
+    return collection_parsers.BlogParser.process(initial_data["response"])
 
 
 def parse_post_list(initial_data):
-    initial_data = initial_data["response"]
-    return collection_parsers.process_post_list(initial_data)
+    return collection_parsers.process_post_list(initial_data["response"])
 
