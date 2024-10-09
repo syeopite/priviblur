76ce023f9ecf9c25e0c0badb09c3ee6fe7928dce
syeopite <syeopite@syeopite.dev>
2024-10-08 18:42:06 -0700

    Add logic to parse reply notes

--- src/priviblur_extractor/parse/__init__.py
+++ src/priviblur_extractor/parse/__init__.py
@@ -1,1 +1,1 @@
-from .base import parse_timeline, parse_blog_timeline
+from .base import parse_timeline, parse_blog_timeline, parse_note_timeline
--- src/priviblur_extractor/parse/base.py
+++ src/priviblur_extractor/parse/base.py
@@ -10,3 +10,7 @@ def parse_blog_timeline(initial_data, is_search = False):
     else:
         return collection_parsers.BlogTimelineParser.process(initial_data["response"])
 
+
+def parse_note_timeline(initial_data):
+    """Parses a note timeline object"""
+    return collection_parsers.NoteTimelineParser.process(initial_data["response"])
