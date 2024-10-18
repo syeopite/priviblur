8a0d94f0be6584939c9f36c9832d0f037b2740ef
syeopite <syeopite@syeopite.dev>
2024-10-17 19:33:12 -0700

    Overhaul routing logic to check slug before req.
    This commit removes the slug check logic from the former route handlers
    `_blog_post` and `_blog_post_no_slug`. Replacing this, is a middleware
    that handles the slug check logic instead.
    
    By using a middleware, we can apply this logic uniformly to all
    additional blog post routes. For example for a post note viewer
    ensuring that the endpoints to view post notes always has the correct
    slug if applicable, and to remove the given slug if unnecessary.
    
    As the logic that separated the slug and slugless post routes have
    been transformed to a single middleware, these two routes have been
    merged to use the same handler.

--- src/routes/blogs/post.py
+++ src/routes/blogs/post.py
@@ -8,9 +8,55 @@ from ... import cache, priviblur_extractor
 blog_post_bp = sanic.Blueprint("blog_post", url_prefix="/<post_id:int>")
 
 
-async def render_blog_post(request, blog, post):
-    """Handles the logic for rendering viewing a single blog post"""
-    blog_info = priviblur_extractor.models.timelines.BlogTimeline(post.blog, (), None, None)
+def get_blog_post_path(request):
+    """Returns the path to the user requested blog post endpoint"""
+    return f"/{'/'.join(str(path_component) for path_component in request.match_info.values())}"
+
+
+@blog_post_bp.on_request
+async def before_blog_post_request(request):
+    """Validates that the user requested endpoint contains a valid slug
+
+    Redirects to a valid URL if otherwise
+    """
+    # Ensure that the slug is correct when present
+    blog = urllib.parse.unquote(request.match_info["blog"])
+    post_id = request.match_info["post_id"]
+
+    post = (await cache.get_blog_post(request.app.ctx, blog, post_id)).elements[0]
+
+    # Check if slug is passed
+    if slug := request.match_info.get("slug"):
+        # Redirect to the correct slug if the given slug does not match the post's slug
+        if slug != post.slug:
+            # Unless of course the post doesn't have a slug in the first place in which
+            # we'll redirect to the path without a slug
+            if not post.slug:
+                del request.match_info["slug"]
+                return sanic.redirect(get_blog_post_path(request))
+            else:
+                # Correct post slug and redirect
+                request.match_info["slug"] = post.slug
+                return sanic.redirect(get_blog_post_path(request))
+    elif post.slug:
+        # If the post has a slug but a slug is not given then we'll need to redirect
+        # the user to a path with the slug due to how Tumblr works.
+
+        current_path = [str(path_component) for path_component in request.match_info.values()]
+
+        # Current path is `/<blog>/<post_id>/*`
+        # as such the slug needs to be inserted at position 2
+        current_path.insert(2, post.slug)
+
+        return sanic.redirect(f"/{'/'.join(current_path)}")
+
+    request.ctx.parsed_post = post
+
+
+@blog_post_bp.get("/")
+@blog_post_bp.get("/<slug:str>", name="_blog_post_with_slug")
+async def _blog_post(request: sanic.Request, **kwargs):
+    blog_info = priviblur_extractor.models.timelines.BlogTimeline(request.ctx.parsed_post.blog, (), None, None)
 
     if request.args.get("fetch_polls") in ("1", "true"):
         fetch_poll_results = True
@@ -22,37 +68,7 @@ async def render_blog_post(request, blog, post):
         context={
             "app": request.app,
             "blog": blog_info,
-            "element": post,
+            "element": request.ctx.parsed_post,
             "request_poll_data" : fetch_poll_results,
         }
     )
-
-# Single post
-
-@blog_post_bp.get("/")
-async def _blog_post_no_slug(request: sanic.Request, blog: str, post_id: str):
-    blog = urllib.parse.unquote(blog)
-    post = (await cache.get_blog_post(request.app.ctx, blog, post_id)).elements[0]
-
-    if post.slug:
-        return sanic.redirect(request.app.url_for("blogs._blog_post", blog=blog, post_id=post_id, slug=post.slug, **request.args))
-    else:
-        return await render_blog_post(request, blog, post)
-
-
-@blog_post_bp.get("/<slug:str>")
-async def _blog_post(request: sanic.Request, blog: str, post_id: str, slug: str):
-    blog = urllib.parse.unquote(blog)
-    slug = urllib.parse.unquote(slug)
-
-    post = (await cache.get_blog_post(request.app.ctx, blog, post_id)).elements[0]
-
-    # Redirect to the correct slug when the given slug does not match the one of the post
-    if post.slug != slug:
-        # Unless of course the slug is empty. In that case we'll remove the slug. 
-        if post.slug:
-            return sanic.redirect(request.app.url_for("blogs._blog_post", blog=blog, post_id=post_id, slug=post.slug, **request.args))
-        else:
-            return sanic.redirect(request.app.url_for("blogs._blog_post_no_slug", blog=blog, post_id=post_id, **request.args))
-    else:
-        return await render_blog_post(request, blog, post)
