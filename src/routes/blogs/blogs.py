0ee65eb7aa7ff1d513ed6a3f7cb53453b0caef53
syeopite <70992037+syeopite@users.noreply.github.com>
2024-10-18 20:23:20 +0000

    Fix redirects for /post/* endpoints (#119)

--- src/routes/blogs/blogs.py
+++ src/routes/blogs/blogs.py
@@ -99,9 +99,9 @@ async def query_param_redirect(request: sanic.Request, blog: str):
 
 @blogs.get("/post/<post_id:int>")
 async def redirect_slash_post_no_slug(request: sanic.Request, blog: str, post_id: str):
-    return sanic.redirect(request.app.url_for("blog_post._blog_post_no_slug", blog=blog, post_id=post_id))
+    return sanic.redirect(request.app.url_for("blog_post._blog_post", blog=blog, post_id=post_id))
 
 
 @blogs.get("/post/<post_id:int>/<slug:str>")
 async def redirect_slash_post(request: sanic.Request, blog: str, post_id: str, slug: str):
-    return sanic.redirect(request.app.url_for("blog_post._blog_post", blog=blog, post_id=post_id, slug=slug))
+    return sanic.redirect(request.app.url_for("blog_post._blog_post_with_slug", blog=blog, post_id=post_id, slug=slug))
