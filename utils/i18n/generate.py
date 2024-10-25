e981634ba8a0786fe4a28931c51aff8fabd09b3b
syeopite <70992037+syeopite@users.noreply.github.com>
2024-10-25 05:48:39 +0000

    Add workflow to generate static locale data (#126)
    Will reduces the time needed to initialize locales on startup once used

---
+++ utils/i18n/generate.py
@@ -0,0 +1,28 @@
+import glob
+import babel
+import polib
+
+locales_to_pofiles = {}
+
+# Parse pofiles so that we may
+for path in glob.glob("locales/*/*/*.po"):
+    segments = path.split("/")
+    locale_name = segments[-3]
+    locales_to_pofiles[locale_name] = polib.pofile(path)
+
+total_us_strings = len(locales_to_pofiles["en_US"])
+
+lines = []
+
+for locale, pofile in locales_to_pofiles.items():
+    language_name = babel.Locale.parse(locale).get_language_name().capitalize()
+    translation_percentage = int((len(pofile.translated_entries())/total_us_strings) * 100)
+
+    lines.append(f"""("{locale}", "{language_name}", {translation_percentage})""")
+
+
+with open("src/helpers/i18n_data.py", "w") as file:
+    file.write("LOCALE_DATA = (\n")
+    for line in lines:
+        file.write(f"    {line},\n")
+    file.write(")")
