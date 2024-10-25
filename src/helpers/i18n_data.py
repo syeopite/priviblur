bf0eb17cb1d8d1411410f473e58af10ca0833986
syeopite <syeopite@users.noreply.github.com>
2024-10-25 07:36:59 +0000

    Regenerate static locale data

--- src/helpers/i18n_data.py
+++ src/helpers/i18n_data.py
@@ -1,10 +1,10 @@
 LOCALE_DATA = {
-    "cs_CZ": ("Čeština", 98),
+    "cs_CZ": ("Čeština", 100),
     "de": ("Deutsch", 37),
     "en_US": ("English", 100),
-    "fr": ("Français", 79),
-    "ja": ("日本語", 74),
+    "fr": ("Français", 80),
+    "ja": ("日本語", 75),
     "nb_NO": ("Norsk bokmål", 21),
-    "uk": ("Українська", 98),
+    "uk": ("Українська", 100),
     "zh_Hans": ("中文", 0),
 }
