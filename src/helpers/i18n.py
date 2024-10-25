88232bbc9f350bb58b06360e404ec104d6fe0260
syeopite <syeopite@syeopite.dev>
2024-10-24 23:20:06 -0700

    Use static generated locale data for language name

--- src/helpers/i18n.py
+++ src/helpers/i18n.py
@@ -2,8 +2,18 @@ import sys
 import gettext
 import typing
 
-import babel
 import sanic
+
+from .i18n_data import LOCALE_DATA
+
+
+class Language:
+    """Stores metadata about supported translations"""
+    def __init__(self, locale, gettext_instance,) -> None:
+        self.locale = locale
+        self.instance = gettext_instance
+
+        self.name, self.translation_percentage = LOCALE_DATA[locale]
 
 SUPPORTED_LANGUAGES = [
     "en_US", "cs_CZ", "fr", "ja", "uk"
@@ -11,19 +21,16 @@ SUPPORTED_LANGUAGES = [
 
 SUPPORTED_LANGUAGES.sort()
 
-LANGUAGE_NAMES = {
-    locale : babel.Locale.parse(locale).get_language_name().capitalize() for locale in SUPPORTED_LANGUAGES
-}
 
 def initialize_locales() -> typing.Mapping[str, gettext.GNUTranslations]:
     """Initializes locales into GNUTranslations instances"""
     # Initialize locales
     try:
-        gettext_instances = {}
-        for language in SUPPORTED_LANGUAGES:
-            gettext_instances[language] = gettext.translation(
-                "priviblur", localedir="locales", languages=(language,)
-            )
+        languages = {}
+        for locale in SUPPORTED_LANGUAGES:
+            instance = gettext.translation("priviblur", localedir="locales", languages=(locale,))
+
+            languages[locale] = Language(locale, instance)
     except FileNotFoundError as e:
         print(
             'Error: Unable to find locale files. '
@@ -34,14 +41,14 @@ def initialize_locales() -> typing.Mapping[str, gettext.GNUTranslations]:
     except Exception as e:
         raise e
 
-    return gettext_instances
+    return languages
 
 
 def translate(language : str, id : str, number : int|float = None,
               substitution : str = None) -> str:
     app = sanic.Sanic.get_app("Priviblur")
 
-    gettext_instance = app.ctx.GETTEXT_INSTANCES[language]
+    gettext_instance = app.ctx.LANGUAGES[language].instance
 
     if number is not None:
         translated = gettext_instance.ngettext(id, f"{id}_plural", number)
