dcf908aca3f3ffca6a8a8e45e2bcc6f1d32a4fff
syeopite <syeopite@syeopite.dev>
2024-10-24 23:11:50 -0700

    Use dictionary instead for static locale data

--- utils/i18n/generate.py
+++ utils/i18n/generate.py
@@ -10,19 +10,19 @@ for path in glob.glob("locales/*/*/*.po"):
     locale_name = segments[-3]
     locales_to_pofiles[locale_name] = polib.pofile(path)
 
-total_us_strings = len(locales_to_pofiles["en_US"])
+total_english_strings = len(locales_to_pofiles["en_US"])
 
 lines = []
 
 for locale, pofile in locales_to_pofiles.items():
     language_name = babel.Locale.parse(locale).get_language_name().capitalize()
-    translation_percentage = int((len(pofile.translated_entries())/total_us_strings) * 100)
+    translation_percentage = int((len(pofile.translated_entries())/total_english_strings) * 100)
 
-    lines.append(f"""("{locale}", "{language_name}", {translation_percentage})""")
+    lines.append(f""""{locale}": ("{language_name}", {translation_percentage})""")
 
 
 with open("src/helpers/i18n_data.py", "w") as file:
-    file.write("LOCALE_DATA = (\n")
+    file.write("LOCALE_DATA = {\n")
     for line in lines:
         file.write(f"    {line},\n")
-    file.write(")")
+    file.write("}")
