7b1db7ccbe95bdc0450055c88d8b5b6f507f93e4
syeopite <syeopite@syeopite.dev>
2024-11-29 17:36:04 -0800

    Use language display name instead of lang name
    Allows differentiating between different types of
    the same language. Eg Chinese simplified and traditional
    
    Needs better UI/UX.

--- src/helpers/i18n_data.py
+++ src/helpers/i18n_data.py
@@ -1,12 +1,12 @@
 LOCALE_DATA = {
-    "cs_CZ": ("Čeština", 100),
+    "cs_CZ": ("Čeština (česko)", 100),
     "de": ("Deutsch", 37),
-    "en_US": ("English", 100),
+    "en_US": ("English (united states)", 100),
     "es": ("Español", 100),
     "fr": ("Français", 80),
     "ja": ("日本語", 75),
-    "nb_NO": ("Norsk bokmål", 21),
+    "nb_NO": ("Norsk bokmål (norge)", 21),
     "uk": ("Українська", 100),
-    "zh_Hans": ("中文", 54),
-    "zh_Hant": ("中文", 76),
+    "zh_Hans": ("中文 (简体)", 54),
+    "zh_Hant": ("中文 (繁體)", 76),
 }
