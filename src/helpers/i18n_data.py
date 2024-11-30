9ee07f0e59945b05ff0e19f3bd6a3184079f00d5
syeopite <syeopite@syeopite.dev>
2024-11-29 17:10:04 -0800

    Enable support for new languages
    Spanish
    Chinese (Traditional Han script)
    Chinese (Simplified Han script)

--- src/helpers/i18n_data.py
+++ src/helpers/i18n_data.py
@@ -2,9 +2,11 @@ LOCALE_DATA = {
     "cs_CZ": ("Čeština", 100),
     "de": ("Deutsch", 37),
     "en_US": ("English", 100),
+    "es": ("Español", 100),
     "fr": ("Français", 80),
     "ja": ("日本語", 75),
     "nb_NO": ("Norsk bokmål", 21),
     "uk": ("Українська", 100),
-    "zh_Hans": ("中文", 0),
+    "zh_Hans": ("中文", 54),
+    "zh_Hant": ("中文", 76),
 }
