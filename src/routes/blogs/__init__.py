fd2a70f400fd0e06041321d99541aa85543ef75b
jollySleeper <git@pm21.anonaddy.com>
2025-06-01 08:35:55 +0530

    fix: regex digits

--- src/routes/blogs/__init__.py
+++ src/routes/blogs/__init__.py
@@ -3,5 +3,5 @@ from sanic import Blueprint
 from . import blogs, post
 
 blogs_group = Blueprint.group(
-    blogs.blogs, post.blog_post_bp, url_prefix="/<blog:([a-z\d]{1}[a-z\d-]{0,30}[a-z\d]{0,1})>"
+    blogs.blogs, post.blog_post_bp, url_prefix=r"/<blog:([a-z\d]{1}[a-z\d-]{0,30}[a-z\d]{0,1})>"
 )
