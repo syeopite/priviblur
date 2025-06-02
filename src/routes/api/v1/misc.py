43a6c44e21b8b65fb2054a865421a7a1fe989b75
jollySleeper <87367804+jollySleeper@users.noreply.github.com>
2025-06-02 06:37:01 +0530

    fix regex digits warnings (#250)
    * fix: regex digits
    
    * style(formatting): using ruff format

--- src/routes/api/v1/misc.py
+++ src/routes/api/v1/misc.py
@@ -7,7 +7,9 @@ from ....cache import get_poll_results
 misc = sanic.Blueprint("api_misc", url_prefix="/")
 
 
-@misc.get("/poll/<blog:([a-z\d]{1}[a-z\d-]{0,30}[a-z\d]{0,1})>/<post_id:int>/<poll_id:str>/results")
+@misc.get(
+    r"/poll/<blog:([a-z\d]{1}[a-z\d-]{0,30}[a-z\d]{0,1})>/<post_id:int>/<poll_id:str>/results"
+)
 async def poll_results(request, blog: str, post_id: int, poll_id: int):
     blog = urllib.parse.unquote(blog)
     poll_id = urllib.parse.unquote(poll_id)
